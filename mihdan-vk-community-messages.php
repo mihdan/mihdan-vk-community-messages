1163f3991857e1d7248f78716403ddde7811d38a
mihdan <mikhail@kobzarev.com>
2017-06-03 16:22:45 +0300

    Настройки плагина начал переносить в кастомайзер как советуют кор-девы

--- mihdan-vk-community-messages.php
+++ mihdan-vk-community-messages.php
@@ -126,8 +126,9 @@ if ( ! class_exists( 'Mihdan_Vk_Community_Messages' ) ) {
 		private function hooks() {
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 			add_action( 'wp_footer', array( $this, 'insert_placeholder' ) );
-			add_action( 'admin_menu', array( $this, 'admin_menu' ) );
-			add_action( 'admin_init', array( $this, 'register_settings' ) );
+			//add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+			//add_action( 'admin_init', array( $this, 'register_settings' ) );
+			add_action( 'customize_register', array( $this, 'customize_register' ) );
 		}
 
 		/**
@@ -148,28 +149,105 @@ if ( ! class_exists( 'Mihdan_Vk_Community_Messages' ) ) {
 		}
 
 		/**
+		 * @param WP_Customize_Manager $wp_customize
+		 */
+		public function customize_register( $wp_customize ) {
+
+			// 1. Добавить секцию
+			$wp_customize->add_section( $this->slug,
+				array(
+					'title'       => __( 'VK Community Messages', $this->slug ),
+					//'priority'    => 35,
+					'capability'  => 'edit_theme_options',
+					'description' => __( 'Allows you to customize some example settings for MyTheme.', $this->slug ),
+				)
+			);
+
+			$wp_customize->add_setting(
+				$this->slug . '_options[welcome_screen]',
+				array(
+					'default'    =>  'true',
+					'type' => 'option',
+					//'transport'  =>  'postMessage'
+				)
+			);
+
+			$wp_customize->add_control(
+				'welcome_screen',
+				array(
+					'section'   => $this->slug,
+					'settings' => $this->slug . '_options[welcome_screen]',
+					'label'     => 'Экран приветствия',
+					'description'     => 'Информация о том, нужно ли показывать экран приветствия',
+					'type'      => 'checkbox'
+				)
+			);
+
+			$wp_customize->add_setting(
+				$this->slug . '_options[expanded]',
+				array(
+					'default'    =>  'true',
+					'type' => 'option',
+					//'transport'  =>  'postMessage'
+				)
+			);
+
+			$wp_customize->add_control(
+				'expanded',
+				array(
+					'section'   => $this->slug,
+					'settings' => $this->slug . '_options[expanded]',
+					'label'     => 'Раскрытие',
+					'description'     => 'Если нужно раскрыть виджет сразу;',
+					'type'      => 'checkbox'
+				)
+			);
+
+			//2. Register new settings to the WP database...
+//			$wp_customize->add_setting( 'link_textcolor', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
+//				array(
+//					'default'    => '#2BA6CB', //Default setting/value to save
+//					'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
+//					'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
+//					'transport'  => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
+//				)
+//			);
+//
+//			//3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
+//			$wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
+//				$wp_customize, //Pass the $wp_customize object (required)
+//				'mytheme_link_textcolor', //Set a unique ID for the control
+//				array(
+//					'label'      => __( 'Link Color', 'mytheme' ), //Admin-visible name of the control
+//					'settings'   => 'link_textcolor', //Which setting to load and manipulate (serialized is okay)
+//					'priority'   => 10, //Determines the order this control appears in for the specified section
+//					'section'    => $this->slug, //ID of the section this control should render in (can be one of yours, or a WordPress default section)
+//				)
+//			) );
+		}
+
+		/**
 		 * Добавить настройки плагина
 		 * в меню админки.
 		 */
-		public function admin_menu() {
-			add_options_page(
-				'Настройки',
-				'VK Community Messages',
-				'manage_options',
-				$this->slug,
-				array( $this, 'options_page' )
-			);
-		}
+//		public function admin_menu() {
+//			add_theme_page(
+//				'Настройки',
+//				'VK Community Messages',
+//				'edit_theme_options',
+//				'customize.php'
+//			);
+//		}
 
-		public function register_settings() {}
+//		public function register_settings() {}
 
 		/**
 		 * Подключить шаблоны вывода
 		 * страницы настроек.
 		 */
-		public function options_page() {
-			require_once ( $this->dir_path . 'admin/options.php' );
-		}
+//		public function options_page() {
+//			require_once ( $this->dir_path . 'admin/options.php' );
+//		}
 	}
 
 	function mihdan_vk_community_messages() {
@@ -180,5 +258,4 @@ if ( ! class_exists( 'Mihdan_Vk_Community_Messages' ) ) {
 	// чтобы можно было переопределять настройки в самой теме сайта.s
 	add_action( 'after_setup_theme', 'mihdan_vk_community_messages' );
 }
-
 // eof
